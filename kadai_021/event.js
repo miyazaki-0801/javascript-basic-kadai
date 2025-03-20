f51dca8df40b81e035c757fc60e4d9603ea7f240
miyazaki-0801 <s.m.h.1.8.1.dg@icloud.com>
2025-03-20 19:29:31 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn")
+
+const text = document.getElementById("text")
+
+btn.addEventListener("click", function() {
+  setTimeout(function() {
+      text.textContent = "ボタンをクリックしました。";
+  }, 2000);
+
+});
